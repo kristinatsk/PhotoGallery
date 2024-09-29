faffd3e6ea3dce6879b921e81b7579ee6ee0c6a7
kristinatsk <kristinatsk@gmail.com>
2024-09-29 21:19:38 +0300

    Initial Commit

---
+++ Milestone Projects 10-12/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Milestone Projects 10-12
+//
+//  Created by Kristina Kostenko on 29.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
